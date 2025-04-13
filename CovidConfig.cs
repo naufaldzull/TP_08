9bddd411b6e7aea8363fc8cf652f92980d35a94b
naufa <naufal.mdzulfik@gmail.com>
2025-04-13 22:21:49 +0700

    menyelesaikan program dengan runtime configuration

---
+++ CovidConfig.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace tpmodul8_103022300021
+{
+    class CovidConfig
+    {
+        public string satuan_suhu { get; set; }
+        public int batas_hari_deman { get; set; }
+        public string pesan_ditolak { get; set; }
+        public string pesan_diterima { get; set; }
+
+        private const string configFile = "covid_config.json";
+
+        public static CovidConfig Load()
+        {
+            if (!File.Exists(configFile))
+            {
+                var defaultConfig = new CovidConfig
+                {
+                    satuan_suhu = "celcius",
+                    batas_hari_deman = 14,
+                    pesan_ditolak = "Anda tidak diperbolehkan masuk ke dalam gedung ini",
+                    pesan_diterima = "Anda dipersilahkan untuk masuk ke dalam gedung ini"
+                };
+                File.WriteAllText(configFile, JsonSerializer.Serialize(defaultConfig));
+                return defaultConfig;
+            }
+
+            string json = File.ReadAllText(configFile);
+            return JsonSerializer.Deserialize<CovidConfig>(json);
+        }
+
+        public void UbahSatuan()
+        {
+            satuan_suhu = satuan_suhu == "celcius" ? "fahrenheit" : "celcius";
+            File.WriteAllText(configFile, JsonSerializer.Serialize(this));
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,67 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using tpmodul8_103022300021;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        CovidConfig config = CovidConfig.Load();
+
+        Console.Write($"Berapa suhu badan anda saat ini? Dalam nilai {config.satuan_suhu}: ");
+        double suhu1 = double.Parse(Console.ReadLine());
+
+        Console.Write("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala demam? ");
+        int hariDemam1 = int.Parse(Console.ReadLine());
+
+        bool suhuNormal1 = false;
+
+        if (config.satuan_suhu == "celcius")
+        {
+            suhuNormal1 = suhu1 >= 36.5 && suhu1 <= 37.5;
+        }
+        else if (config.satuan_suhu == "fahrenheit")
+        {
+            suhuNormal1 = suhu1 >= 97.7 && suhu1 <= 99.5;
+        }
+
+        if (suhuNormal1 && hariDemam1 < config.batas_hari_deman)
+        {
+            Console.WriteLine(config.pesan_diterima);
+        }
+        else
+        {
+            Console.WriteLine(config.pesan_ditolak);
+        }
+
+        // Panggil method ubah satuan
+        config.UbahSatuan();
+        Console.WriteLine($"Suhu diubah menjadi {config.satuan_suhu}...");
+
+        Console.Write($"Berapa suhu badan anda saat ini? Dalam nilai {config.satuan_suhu}: ");
+        double suhu2 = double.Parse(Console.ReadLine());
+
+        Console.Write("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala demam? ");
+        int hariDemam2 = int.Parse(Console.ReadLine());
+
+        bool suhuNormal2 = false;
+
+        if (config.satuan_suhu == "celcius")
+        {
+            suhuNormal2 = suhu2 >= 36.5 && suhu2 <= 37.5;
+        }
+        else if (config.satuan_suhu == "fahrenheit")
+        {
+            suhuNormal2 = suhu2 >= 97.7 && suhu2 <= 99.5;
+        }
+
+        if (suhuNormal2 && hariDemam2 < config.batas_hari_deman)
+        {
+            Console.WriteLine(config.pesan_diterima);
+        }
+        else
+        {
+            Console.WriteLine(config.pesan_ditolak);
+        }
+    }
+}
+
